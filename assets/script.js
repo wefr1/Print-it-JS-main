2b4549277348ec1a17daabae3cf8f8a1e422c517
wefr1 <weshdzdz1@gmail.com>
2024-01-10 13:26:12 +0100

    Mise à jour du fichier

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,27 @@
 const slides = [
-	{
-	  image: "slide1.jpg",
-	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-	},
-	{
-	  image: "slide2.jpg",
-	  tagLine:
-		"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-	},
-	{
-	  image: "slide3.jpg",
-	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	},
-	{
-	  image: "slide4.png",
-	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-	},
-  ];
-  
-  
-//Initialisation de l'image courante à 0
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// Initialisation de l'image courante à 0
 let currentimg = 0;
 
-//Récupération des images et des textes
+// Récupération des images et des textes
 function sliderImg(n) {
   document.querySelector(".banner-img").src =
     "./assets/images/slideshow/" + slides[n].image;
@@ -30,42 +29,66 @@ function sliderImg(n) {
 }
 
 // Initialisation de la variable pour les "dots"
-let dots = document.querySelector(".dots");
+const dots = document.querySelector(".dots");
 
 // Récupère les boutons de navigation
 const arrowleft = document.querySelector(".arrow_left");
 const arrowright = document.querySelector(".arrow_right");
 
-//Ajout d'un Event Listeners sur la flèche gauche
+// Ajout d'un Event Listeners sur la flèche gauche
 arrowleft.addEventListener("click", () => {
   console.log("j'ai cliqué à gauche");
 
-  //Conditions pour afficher le slide précédent
-  //Si il ne sagit pas de la dernière image du tableau on passe à l'image précédente 
-  //Si on atteint la dernière image du tableau on revient à la première image
+  // Conditions pour afficher le slide précédent
+  // Si ce n'est pas la dernière image du tableau, on passe à l'image précédente
+  // Si on atteint la dernière image du tableau, on revient à la première image
   if (currentimg < slides.length) {
     currentimg--;
   }
   if (currentimg < 0) {
     currentimg = slides.length - 1;
   }
-  sliderimg(currentimg);
+  sliderImg(currentimg);
   navdots(currentimg);
 });
 
-//Ajout d'un Event Listeners sur la flèche droite
+// Ajout d'un Event Listeners sur la flèche droite
 arrowright.addEventListener("click", () => {
   console.log("j'ai cliqué à droite");
 
-  //Conditions pour afficher le slide suivant
-  //Si il ne s'agit pas de la dernière image du tableau on passe à l'image suivante 
-  //Si on atteint la dernière image du tableau on revient à la première image
+  // Conditions pour afficher le slide suivant
+  // Si ce n'est pas la dernière image du tableau, on passe à l'image suivante
+  // Si on atteint la dernière image du tableau, on revient à la première image
   if (currentimg < slides.length) {
     currentimg++;
   }
   if (currentimg === slides.length) {
     currentimg = 0;
   }
-  sliderimg(currentimg);
+  sliderImg(currentimg);
   navdots(currentimg);
 });
+
+// Boucle pour créer les bullet points en fonction du nombre de slides
+for (let i = 0; i < slides.length; i++) {
+  let dot = document.createElement("div");
+  dot.classList.add("dot");
+  dots.appendChild(dot);
+
+  if (i === 0) {
+    dot.classList.add("dot_selected");
+  }
+  // Ajout d'un écouteur d'événement sur chaque bullet point
+  dot.addEventListener("click", () => {
+    console.log("clickdot");
+  });
+}
+
+// Ajout de la classe active sur le bullet point qui signale le slide en cours de visionnage
+function navdots(n) {
+  const dotarray = document.querySelectorAll(".dot");
+  dotarray.forEach(function (dot) {
+    dot.classList.remove("dot_selected");
+  });
+  dotarray[n].classList.add("dot_selected");
+}
